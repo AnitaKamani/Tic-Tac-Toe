a4e81ac46ea99399629fa303abc93e9e8be6b26b
AnitaKamani <aniakamani@gmail.com>
2021-12-21 19:38:10 +0330

    Updates

---
+++ precache-manifest.b84df52f84e81123a250654817ab256a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7631aa934bd68dde39e",
+    "url": "/Tic-Tac-Toe/static/css/main.35266508.chunk.css"
+  },
+  {
+    "revision": "e7631aa934bd68dde39e",
+    "url": "/Tic-Tac-Toe/static/js/main.e7631aa9.chunk.js"
+  },
+  {
+    "revision": "01d52df27a56fce8584c",
+    "url": "/Tic-Tac-Toe/static/js/1.01d52df2.chunk.js"
+  },
+  {
+    "revision": "88893e27722ab3ac467f",
+    "url": "/Tic-Tac-Toe/static/js/2.88893e27.chunk.js"
+  },
+  {
+    "revision": "3c69a39169cedcd16dd2",
+    "url": "/Tic-Tac-Toe/static/js/runtime~main.3c69a391.js"
+  },
+  {
+    "revision": "60d4e9f113a7ce66041f6bff975b1516",
+    "url": "/Tic-Tac-Toe/index.html"
+  }
+];
